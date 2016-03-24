330bdc649aeca1cf74717f4a709499dc80fff18a
asdera <poiuytandrew@gmail.com>
2016-03-24 14:07:35 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   $(function() { 
     $('.banner').unslider() 
   })
-  $('.vertical-slider').unslider({
-    autoplay: true, 
-    infinite: true 
+  $('.automatic-slider').unslider({
+    autoplay: true 
   });
 });
 
