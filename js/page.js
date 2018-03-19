81ceece812e163d1bba848435d481a5b9964c3dd
asdera <poiuytandrew@gmail.com>
2018-03-19 00:02:17 -0400

    A WHOLE NEW PRETTY WITTY WEBSITEgit status!

---
+++ js/page.js
@@ -0,0 +1,80 @@
+var parallaxSpeed = 0.5;
+var $root = $('html, body');
+
+archive = [
+
+]
+
+$(document).scroll(function() {
+	$("#mountain").css("background-position-y", document.documentElement.scrollTop*parallaxSpeed+"px");
+	$("#contact").css("background-position-y", (document.documentElement.scrollTop*parallaxSpeed-$("#contact").height())+"px");
+});
+
+$(document).ready(function() {
+	$("a[href='#']").click(function() {
+		$root.animate({ scrollTop: $("#"+$(this).attr("link")).offset().top }, 500);
+
+		return false; // prevents page from reloading
+	});
+	$(".project").hover(function() {
+		$(this).parent("td").animate({
+		    backgroundColor: "#cfdff9"
+		}, 150);
+		$(this).parent("th").animate({
+		    backgroundColor: "#ffdddd"
+		}, 150);
+	}, function() {
+	    $(this).parent("td").animate({
+		    backgroundColor: "#a3c6ff"
+		}, 75);
+		$(this).parent("th").animate({
+		    backgroundColor: "#ffaaaa"
+		}, 75);
+	});
+	$("#myName h1").hover(function() {
+		$(this).animate({
+		    fontSize: 128
+		}, 200);
+	}, function() {
+	    $(this).animate({
+		    fontSize: 96
+		}, 100);
+	});
+	$("#myName p").hover(function() {
+		$(this).animate({
+		    fontSize: 64
+		}, 200);
+	}, function() {
+	    $(this).animate({
+		    fontSize: 48
+		}, 100);
+	});
+	$("#myResume").hover(function() {
+		$(this).find(".dot[animate=width]").animate({
+		    opacity: 1,
+		    width: $(this).width() + 60
+		}, 200);
+		$(this).find(".dot[animate=height]").animate({
+		    opacity: 1,
+		    height: $(this).height() + 60
+		}, 200);
+	}, function() {
+	    $(this).find(".dot[animate=width]").animate({
+		    opacity: 0,
+		    width: 10
+		}, 100);
+		$(this).find(".dot[animate=height]").animate({
+		    opacity: 0,
+		    height: 10
+		}, 100);
+	});
+	$(".contacts").hover(function() {
+		$(this).animate({
+		    backgroundSize: "120%"
+		}, 200);
+	}, function() {
+	    $(this).animate({
+		    backgroundSize: "100%"
+		}, 100);
+	});
+});
