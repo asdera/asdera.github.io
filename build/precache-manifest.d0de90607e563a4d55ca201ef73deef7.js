21b1ef25b6b9311b263c64d23f8101a39488382e
unknown <poiuytandrew@gmail.com>
2020-01-19 23:53:41 -0500

    first build

---
+++ build/precache-manifest.d0de90607e563a4d55ca201ef73deef7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f7a7a2905378be921e6e8a21fe1a48",
+    "url": "/static/media/north.a4f7a7a2.png"
+  },
+  {
+    "revision": "750d313ae27985ef0bfe",
+    "url": "/static/css/main.8ef8b0af.chunk.css"
+  },
+  {
+    "revision": "686a299e43e78e3b2d94",
+    "url": "/static/js/1.686a299e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3670aa493ee09e92c7b8e1e7c2f5b441",
+    "url": "/static/media/CaviarDreams.3670aa49.ttf"
+  },
+  {
+    "revision": "7b02976656930aebbd3eeff675508874",
+    "url": "/static/media/Genera-AltLight.7b029766.ttf"
+  },
+  {
+    "revision": "3170dd100e041e90da5ca346b45adcc5",
+    "url": "/static/media/fortnite.3170dd10.png"
+  },
+  {
+    "revision": "48bd1dafbdf3ba5c8314e33497046102",
+    "url": "/static/media/scorpion.48bd1daf.png"
+  },
+  {
+    "revision": "3875d3b33e902f94f2ed3c0777397a65",
+    "url": "/static/media/volleyball.3875d3b3.png"
+  },
+  {
+    "revision": "071ed568bf1716375a8b3b80ce9e7071",
+    "url": "/static/media/flipper.071ed568.png"
+  },
+  {
+    "revision": "ba275aa7022dadb8a866798e03c36d06",
+    "url": "/static/media/bhoppers.ba275aa7.png"
+  },
+  {
+    "revision": "750d313ae27985ef0bfe",
+    "url": "/static/js/main.750d313a.chunk.js"
+  },
+  {
+    "revision": "4e05c1d252c878c049694b6fe15c7033",
+    "url": "/static/media/acers.4e05c1d2.png"
+  },
+  {
+    "revision": "04f8c0e33f32f3fac06580f300ea90ed",
+    "url": "/static/media/starterhacks.04f8c0e3.png"
+  },
+  {
+    "revision": "adbc5e91dde7859cf9d42a1b232e985f",
+    "url": "/static/media/gridiron.adbc5e91.png"
+  },
+  {
+    "revision": "1e6222735b2efba2ba70bec28716411d",
+    "url": "/static/media/neon.1e622273.png"
+  },
+  {
+    "revision": "50c9a1062cf025adf5cd1acc146229cc",
+    "url": "/static/media/hexagon.50c9a106.png"
+  },
+  {
+    "revision": "9a92969f56cc62e79d2878f369785fa3",
+    "url": "/static/media/qcsys.9a92969f.png"
+  },
+  {
+    "revision": "6ca1f6d940f0e8808be0ccb0ec699eed",
+    "url": "/static/media/jamhacks.6ca1f6d9.png"
+  },
+  {
+    "revision": "67f0bf09d18a1bf6a7204235d552781f",
+    "url": "/static/media/enterprise.67f0bf09.png"
+  },
+  {
+    "revision": "6de48bcccf579779d8e4d4ef20984608",
+    "url": "/static/media/shad.6de48bcc.png"
+  },
+  {
+    "revision": "90b279f7c1782a2f57de06a0c2cc22a7",
+    "url": "/index.html"
+  }
+];
